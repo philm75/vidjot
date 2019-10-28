80d69d1369c15803507204910ceb976ab1dc174d
Phil <phil.merrilees@gmail.com>
2019-10-28 20:02:27 +0000

    Initial Commit

---
+++ config/database.js
@@ -0,0 +1,9 @@
+if (process.env.NODE_ENV === 'prod') {
+    module.exports = {
+        mongoURI: 'mongodb+srv://phil-merrilees:cjv1975@cluster0-wwjld.mongodb.net/test?retryWrites=true&w=majority'
+    } 
+} else {
+    module.exports = {
+        mongoURI: 'mongodb://localhost/vidjot-dev'
+    }
+}
