094ee89cc2d0d97ad9201b4f81d7676b47bf40c5
Phil <phil.merrilees@gmail.com>
2019-10-28 20:29:43 +0000

    Add correct env name

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'prod') {
+if (process.env.NODE_ENV === 'production') {
     module.exports = {
         mongoURI: 'mongodb+srv://phil-merrilees:cjv1975@cluster0-wwjld.mongodb.net/test?retryWrites=true&w=majority'
     } 
